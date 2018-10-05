148eddfdede94384bdabf1bc8734da8725013ff5
Ruben Reyes <ruben.reyes@lyquix.com>
2018-10-05 12:08:42 -0400

    Initial commit

---
+++ css/css.dist.sh
@@ -0,0 +1,20 @@
+###
+#
+# css.dist.sh - Bash script to process SCSS files, run autoprefixer, and chunk files
+#
+# @version     2.0.0
+# @package     lyquix_html
+# @author      Lyquix
+# @copyright   Copyright (C) 2015 - 2018 Lyquix
+# @license     GNU General Public License version 2 or later
+# @link        https://github.com/Lyquix/lyquix_html
+#
+###
+
+# Get script directory
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" > /dev/null && pwd )"
+cd $DIR
+
+sass ./styles.scss > ./styles.css
+postcss -u autoprefixer --autoprefixer.browsers "> 0.5%, last 3 versions" -r ./styles.css
+uglifycss ./styles.css > ./styles.min.css
---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * index.php - Main template file, calls custom project template
+ *
+ * @version     2.0.0
+ * @package     lyquix_html
+ * @author      Lyquix
+ * @copyright   Copyright (C) 2015 - 2018 Lyquix
+ * @license     GNU General Public License version 2 or later
+ * @link        https://github.com/Lyquix/lyquix_html
+ */
+
+// No direct access
+defined('_JEXEC') or die('Restricted access');
+
+/**
+*** NOTICE: Do not modify this file!
+*** If you need to customize your template, create a file named custom.php
+*** You may rename the included file custom.dist.php
+**/
+
+if(file_exists(__DIR__ . '/custom.php')) :
+	require __DIR__ . '/custom.php';
+else :
+?><!DOCTYPE html>
+<html>
+	<body>
+		File <span style="font-family: monospace;"><?php echo JPATH_BASE . '/templates/' . $this->template; ?>/custom.php</span> not found.
+	</body>
+</html>
+<?php endif; // endif for including custom.php
